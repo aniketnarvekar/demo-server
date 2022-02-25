8838a9105df7d971f87367809919a21e6f5fc5d8
Aniket Narvekar <aniket.narvekar0496@gmail.com>
2022-02-26 01:55:58 +0530

    changes

--- app.lisp
+++ app.lisp
@@ -1,8 +1,3 @@
 (push #p"/app/" asdf:*central-registry*)
 
 (ql:quickload :demo-server)
-
-(demo-server::clackup
- (lambda (env)
-   (declare (ignore env))
-   '(200 (:content-type "text/plain") ("hello, Clack!"))))
