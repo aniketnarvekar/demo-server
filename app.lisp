555aa7968dfaa299b174868d5c9723bda2b816fd
Aniket Narvekar <aniket.narvekar0496@gmail.com>
2022-02-26 02:12:34 +0530

    handler

--- app.lisp
+++ app.lisp
@@ -1,3 +1,7 @@
 (push #p"/app/" asdf:*central-registry*)
 
 (ql:quickload :demo-server)
+
+(hunchentoot:define-easy-handler (greeting :uri "/") ()
+  (setf (hunchentoot:content-type* "text/plain"))
+  "hello World!!!")
